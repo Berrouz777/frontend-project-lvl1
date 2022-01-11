f0f4c3b42b522fd898f07b799896b1889b378d3b
Squire <esq.berrouz@gmail.com>
2022-01-11 20:47:02 +0500

    add a separate progression function

--- src/games/logic-game-progression.js
+++ src/games/logic-game-progression.js
@@ -3,19 +3,27 @@ import getRandomNumber from '../getRandomNumber.js';
 
 const gameRule = 'What number is missing in the progression?';
 
+const getProgression = (progressionStart, progressionStep, progressionLength) => {
+  const result = [];
+  const progressionLastNumber = progressionStart + (progressionStep * progressionLength);
+
+  for (let i = progressionStart; i < progressionLastNumber; i += progressionStep) {
+    result.push(`${i}`);
+  }
+
+  return result;
+};
+
 const logicProgression = () => {
   const firstNumber = getRandomNumber(1, 99);
   const step = getRandomNumber(2, 10);
   const numbersLength = getRandomNumber(5, 10);
 
-  const result = [];
-  for (let i = firstNumber; i < firstNumber + (step * numbersLength); i += step) {
-    result.push(`${i}`);
-  }
+  const progression = getProgression(firstNumber, step, numbersLength);
 
-  const correctAnswer = String(result[Math.floor(Math.random() * result.length)]);
-  result[result.indexOf(correctAnswer)] = '..';
-  const question = result.join(' ');
+  const correctAnswer = String(progression[Math.floor(Math.random() * progression.length)]);
+  progression[progression.indexOf(correctAnswer)] = '..';
+  const question = progression.join(' ');
 
   return [question, correctAnswer];
 };
